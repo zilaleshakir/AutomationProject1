974baf2d5a65920efec771ba665091db6c401c63
zilaleshakir <zilaleshakir@gmail.com>
2022-08-10 15:24:44 +0300

    added more hello feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,6 +3,8 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("HelloTest");
         System.out.println("Developing hello feature");
+        System.out.println("Local Repo is already published to GitHub");
+        System.out.println("Doing more work");
 
 
     }
