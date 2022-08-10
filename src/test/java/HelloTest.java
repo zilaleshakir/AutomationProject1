d042bfc1562aa15aae9260dc256341d8bdf09a23
zilaleshakir <zilaleshakir@gmail.com>
2022-08-10 15:10:19 +0300

    Started hello feature

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,9 @@
+public class HelloTest {
+
+    public static void main(String[] args) {
+        System.out.println("HelloTest");
+        System.out.println("Developing hello feature");
+
+
+    }
+}
